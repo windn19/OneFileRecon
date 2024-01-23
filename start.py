837505f75a4d916862d404e7caf2afd2cbfbe26d
Dmitry Noskov <windn12@gmail.com>
2024-01-23 15:02:57 +0500

    Update start.py

--- start.py
+++ start.py
@@ -1,13 +1,9 @@
-from os import walk, remove, listdir
-from tempfile import NamedTemporaryFile
-
-import cv2
-import numpy as np
+ocr_file = ''
 
 from detect import load_model, run
 
 
-model = load_model(weights='bestR5.pt')
+model = load_model(weights='')
 
 print(run(model=model, source='test_image_part1/', save_crop=True, nosave=True))
 
